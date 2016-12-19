7e166f167c0faa537f2deadd693f9e6add337619
jamesbyrne89 <mail@jamestbyrne.com>
2016-12-19 18:43:53 +0000

    Added colour highlighting for winner and added responsive styling

--- js/app.js
+++ js/app.js
@@ -1,5 +1,3 @@
-
-
 function startGame(){
 		for (i = 1; i <= 9; i++){
 		reset(i);
@@ -9,8 +7,6 @@ function startGame(){
 	document.winner = null;
 	setMessage("Ready? " + document.turn + " goes first")
 }
-
-
 
 function setMessage(msg){
 	document.getElementById("message").innerText = msg;
@@ -34,34 +30,74 @@ function switchTurn(){
 	if (detectWinner(document.turn)) {
 		setMessage(document.turn + " won!")
 		document.winner = document.turn;
+		
 	}
 	else if (turnNo ==  9){
 		setMessage("It's a draw!");
 	}
 	else if(document.turn == "X"){
 		document.turn = "O";
-		setMessage("It's " + document.turn + "'s turn")
+		setMessage(document.turn + ", it's your turn")
 	} else {
 		document.turn = "X";
-		setMessage("It's " + document.turn + "'s turn")
+		setMessage(document.turn + ", it's your turn")
 	}
 
 }
 
 function detectWinner(move) {
 	var result = false;
-	if  (checkForRow(1,2,3,move) ||
-		checkForRow(2,5,8,move) ||
-		checkForRow(3,6,9,move) ||
-		checkForRow(4,5,6,move) ||
-		checkForRow(1,5,9,move) ||
-		checkForRow(3,5,7,move) ||
-		checkForRow(7,8,9,move) ||
-		checkForRow(1,4,7,move)) {
+	if  (checkForRow(1,2,3,move)){
+		result = true;
+		document.getElementById('spot-1').classList.add('winner');
+		document.getElementById('spot-2').classList.add('winner');
+		document.getElementById('spot-3').classList.add('winner');
+	}
+	else if (checkForRow(2,5,8,move)){
+		result = true;
+		document.getElementById('spot-2').classList.add('winner');
+		document.getElementById('spot-5').classList.add('winner');
+		document.getElementById('spot-8').classList.add('winner');
+	}
+	else if (checkForRow(3,6,9,move)){
+		result = true;
+		document.getElementById('spot-3').classList.add('winner');
+		document.getElementById('spot-6').classList.add('winner');
+		document.getElementById('spot-9').classList.add('winner');
+	}
+	else if (checkForRow(4,5,6,move)){
+		result = true;
+		document.getElementById('spot-4').classList.add('winner');
+		document.getElementById('spot-5').classList.add('winner');
+		document.getElementById('spot-6').classList.add('winner');		
+	}
+	else if (checkForRow(1,5,9,move)){
+		result = true;
+		document.getElementById('spot-1').classList.add('winner');
+		document.getElementById('spot-5').classList.add('winner');
+		document.getElementById('spot-9').classList.add('winner');		
+	}
+	else if (checkForRow(3,5,7,move)){
+		result = true;
+		document.getElementById('spot-3').classList.add('winner');
+		document.getElementById('spot-5').classList.add('winner');
+		document.getElementById('spot-7').classList.add('winner');		
+	}
+	else if (checkForRow(7,8,9,move)){
+		result = true;
+		document.getElementById('spot-7').classList.add('winner');
+		document.getElementById('spot-8').classList.add('winner');
+		document.getElementById('spot-9').classList.add('winner');		
+	}
+	else if (checkForRow(1,4,7,move)){
 	result = true;
+		document.getElementById('spot-1').classList.add('winner');
+		document.getElementById('spot-4').classList.add('winner');
+		document.getElementById('spot-7').classList.add('winner');
 	}
 return result;
 }
+
 
 function checkForRow(a, b, c, move) {
 	var result = false;
@@ -71,8 +107,19 @@ function checkForRow(a, b, c, move) {
 	return result;
 }
 
+function highlightWinners(){
+
+}
+
+function getBoxNumber(a, b, c) {
+	console.log(document.getElementById("spot-" + a).id);
+	console.log(document.getElementById("spot-" + b).id);
+	console.log(document.getElementById("spot-" + c).id);
+}
+
 function getBox(number){
 	return document.getElementById("spot-" + number).innerText;
+	
 }
 
 function showReset(){
@@ -91,7 +138,9 @@ function hideReset(){
 
 function reset(number){
 	document.getElementById("spot-" + number).innerText ="";
+	document.getElementById("spot-" + number).classList.remove('winner');
 	turnNo = 0;
+
 }
 
 
@@ -101,3 +150,4 @@ function reset(number){
 
 
 
+ 
